9ee075623433aae9a8ffaa0e7eb1b43a04412259
DogGang <rlaehgusqp@naver.com>
2022-01-17 22:41:47 +0900

    퍼포먼스 안에 performance안에 a태그 링크 정상적으로 수정

---
+++ Readme.txt
@@ -0,0 +1,19 @@
+한림대학교 CODA 중앙동아리 홈페이지입니다.
+
+제작 및 편집 : 빅데이터전공 20195120 김도현
+
+
+웹 사이트 주소 : https://doggang.github.io/coda/
+
+
+
+
+
+
+
+Thanks for downloading this template!
+
+Template Name: Regna
+Template URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ assets/js/main.js
@@ -0,0 +1,293 @@
+/**
+* Template Name: Regna - v4.7.0
+* Template URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
+function call_alert(){
+  swal({
+    title: "회장 연락처",
+    text: "010-2001-6448",
+    icon: "success" //"info,success,warning,error" 중 택1
+  })
+}
+
+function call_null(){
+  swal({
+    title: "비공개",
+    text: "연결된 SNS 링크가 없습니다!",
+    icon: "error" //"info,success,warning,error" 중 택1
+  })
+}
+
+const form = document.querySelector('#hidden');
+const username = document.querySelector('#username');
+const button = document.querySelector('#namebtn');
+
+function prt(event){
+  event.preventDefault();
+  const userName = username.value;
+  if(userName=="김도현"){
+    location.href="../../inner-page.html";
+  }
+}
+
+var lifes = 0;
+
+function life(){
+  lifes+=1;
+  const userName = username.value;
+  if(lifes==3 && userName!="김도현"){
+    username.classList.add('hide');
+    button.classList.add('hide')
+    localStorage.setItem('life','end');
+    swal({
+      title: "실패",
+      text: "3번의 기회가 끝났습니다!\n더이상 시도할 수 없습니다.",
+      icon: "error" //"info,success,warning,error" 중 택1
+    })
+  }else if(life !=3 & userName!="김도현"){
+    swal({
+      title: "틀렸습니다",
+      text: `${3-lifes}번 남았습니다.\n현재 사용한 기회 ${lifes}`,
+      icon: "warning" //"info,success,warning,error" 중 택1
+    })
+    
+  }
+}
+
+function lifeChk(){
+  const remaingLife = localStorage.getItem('life');
+  if(remaingLife === 'end'){
+    username.classList.add('hide');
+    button.classList.add('hide');
+  }
+}
+
+
+
+lifeChk();
+button.addEventListener('click',prt);
+button.addEventListener('click',life);
